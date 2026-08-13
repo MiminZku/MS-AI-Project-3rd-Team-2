e3801565c856d2f7e5f936feb31ab17b07724397
Mingi Kang <rkdalsrl111@naver.com>
2026-08-13 17:45:54 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,7 +41,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+)ㄴㄴㅇㄴㅁㅇㅇㄴㄹ
 
 app.include_router(health.router)
 app.include_router(sessions.router, prefix="/api")
